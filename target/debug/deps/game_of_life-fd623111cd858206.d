9990e064c201629add6337410e870d52abfaf275
Ivan Shafiuddin <87042742+iktefish@users.noreply.github.com>
2021-10-04 00:12:15 +0600

    Finishing draw psudocode

---
+++ target/debug/deps/game_of_life-fd623111cd858206.d
@@ -0,0 +1,5 @@
+/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-fd623111cd858206: src/main.rs
+
+/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-fd623111cd858206.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/game-of-life.d
@@ -0,0 +1,1 @@
+/home/vale/Arcane/p_game-of-life/target/debug/game-of-life: /home/vale/Arcane/p_game-of-life/src/main.rs
