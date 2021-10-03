c176da682e59740354f632a39a30a068d2423de9
Ivan Shafiuddin <87042742+iktefish@users.noreply.github.com>
2021-10-03 21:51:14 +0600

    Created cargo package

---
+++ target/debug/deps/game_of_life-09d74476227c081f.d
@@ -0,0 +1,5 @@
+/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-09d74476227c081f.rmeta: src/main.rs
+
+/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-09d74476227c081f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/game_of_life-3bab8247344037cf.d
@@ -0,0 +1,5 @@
+/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-3bab8247344037cf.rmeta: src/main.rs
+
+/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-3bab8247344037cf.d: src/main.rs
+
+src/main.rs:
