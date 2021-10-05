78837079d13c37b698f96407538dd2d27d8c9783
Ivan Shafiuddin <87042742+iktefish@users.noreply.github.com>
2021-10-06 05:48:17 +0600

    Created new_coordinate() and update_coordiante()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::ffi::CString;
 use std::io;
 use std::collections::HashMap;
+use core::iter;
 
 fn main() {
     let coordinate_val: &str = "x0y0";
@@ -12,7 +14,7 @@ fn main() {
     // );
     // println!("HashMap value {}", data_map["hello"]);
 
-    loop {
+    let grid_size: String = loop {
         let ret_check: (bool, String) = check_input();
 
         if ret_check.0 == true {
@@ -20,9 +22,19 @@ fn main() {
             // println!("From main ---> {:?}, {:?}", ret_check.0, ret_check.1);
 
             println!("Grid dimensions set to {} x {}", ret_check.1, ret_check.1);
-            break;
-        }
-    }
+
+            // These 2 lines wont compile as ret_check.1 dies when loop ends
+            // *grid_size_ptn = ret_check.1.as_str();
+            // *grid_size_ptn = &test_var;
+            break ret_check.1;
+        };
+    };
+    // println!("RUNRUNRUN {}", iterate_through_check());
+
+    println!("grid_size ---> {}", grid_size);
+    let coordinate_tup = update_coordinate(grid_size.parse().unwrap());
+    data_map.insert(&coordinate_tup.0 as &str, ' ');
+    println!("HashMap value {}", data_map[&coordinate_tup.0 as &str]);
  }
 
 fn get_input() -> String {
@@ -47,10 +59,40 @@ fn check_input() -> (bool, String) {
     }
 }
 
+fn update_coordinate(grid_size: i32) -> (String, i32, i32) {
+    // extract_coordinates()
+    let mut hypo_x_idx: i32 = 11;
+    let mut hypo_y_idx: i32 = 13;
+    if hypo_x_idx >= grid_size - 1 {
+        hypo_x_idx = 0;
+        hypo_y_idx = hypo_y_idx + 1;
+    } else {
+        hypo_x_idx = hypo_x_idx + 1;
+    }
+    println!("hypo_x_idx ---> {}", hypo_x_idx);
+    println!("hypo_y_idx ---> {}", hypo_y_idx);
+    // new_coordinate(hypo_x_idx, hypo_y_idx);
+    println!("{}", grid_size);
+
+    let new_coordinate: String = gen_new_coordinate(hypo_x_idx.to_string(), hypo_y_idx.to_string());
+    return (new_coordinate, hypo_x_idx, hypo_y_idx);
+    // return (hypo_x_idx.to_string(), hypo_y_idx.to_string());
+}
+
+fn gen_new_coordinate(x_arg: String, y_arg: String) -> String {
+    let mut new_coordinate: String = "x".to_string();
+    new_coordinate.push_str(&x_arg);
+    new_coordinate.push_str("y");
+    new_coordinate.push_str(&y_arg);
+    println!("{}", new_coordinate);
+    return new_coordinate;
+}
+
+
 // -----------------------------------
 // This function is used to find types
 // Use when needed
 // -----------------------------------
-// fn find_type<T>(_: &T) {
-//     println!("{}", std::any::type_name::<T>())
-// }
+fn find_type<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
