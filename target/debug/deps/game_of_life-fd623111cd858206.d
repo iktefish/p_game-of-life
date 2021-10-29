c599a427cf597ec23f3fd5b922e243c528ac63aa
Ivan Shafiuddin <87042742+iktefish@users.noreply.github.com>
2021-10-29 13:56:47 +0600

    Removed target from git cache

--- target/debug/deps/game_of_life-fd623111cd858206.d
+++
@@ -1,5 +0,0 @@
-/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-fd623111cd858206: src/main.rs
-
-/home/vale/Arcane/p_game-of-life/target/debug/deps/game_of_life-fd623111cd858206.d: src/main.rs
-
-src/main.rs:
