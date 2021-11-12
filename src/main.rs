2cc98ff0d344542a9d4ec244fbe3a1a5714020e5
Ivan Shafiuddin <87042742+iktefish@users.noreply.github.com>
2021-11-12 15:34:36 +0600

    Increased size of static grid & cleaned code

--- src/main.rs
+++ src/main.rs
@@ -3,73 +3,131 @@ use std::{thread, time};
 fn main() {
     let mut grid: Vec<Vec<char>> = gen_grid();
     let mut len = grid.len();
-    
+
     // std_out_grid(&grid);
     let mut x = 0;
     let mut y = 0;
 
     loop {
-        thread::sleep(time::Duration::from_millis(20));
+        thread::sleep(time::Duration::from_millis(10));
         std::process::Command::new("clear").status().unwrap();
         std_out_grid(&grid);
-        println!("x ~~> {}", x);
-        println!("y ~~> {}", y);
+
+        // println!("x ~~> {}", x);
+        // println!("y ~~> {}", y);
+
         let focus_cell = grid[y][x];
 
         let edge_cat = check_edge(x, y, grid.len());
 
         let neighbours = find_neighbour(x, y, &grid, edge_cat);
-        println!("[0] index of neighbours ~~> {}", neighbours[0]);
+
+        // println!("[0] index of neighbours ~~> {}", neighbours[0]);
 
         let life_status = check_life(&focus_cell);
 
         grid = check_fate(x, y, &neighbours, grid);
 
-        if x < len -1 {
+        if x < len - 1 {
             x = x + 1;
-        } else if x == len -1 && y < len-1 {
+        } else if x == len - 1 && y < len - 1 {
             y = y + 1;
             x = len - len;
-        } else if x == len -1 && y == len -1 {
+        } else if x == len - 1 && y == len - 1 {
             x = 0;
             y = 0;
         }
-
-        // for i in grid.iter() {
-        //     for ii in i.iter() {
-        //         if y < len-1 {
-        //             y = y + 1;
-        //             break;
-        //         } else {
-        //             break;
-        //         }
-                
-        //     }
-        //     if y == len-1 {
-
-        //     }
-        // }
-
     }
-    
+
     // println!("grid ~~> {}", grid[y][x]);
     // println!("r_cell ~~> {}", neighbours[0]);
 }
 
 fn gen_grid() -> Vec<Vec<char>> {
-    let row_0: Vec<char> = vec!['*', ' ', '*', '*', '*', '*', '*', ' ', ' ', ' '];
-    let row_1: Vec<char> = vec![' ', '*', ' ', '*', ' ', '*', '*', '*', ' ', ' '];
-    let row_2: Vec<char> = vec!['*', ' ', '*', '*', ' ', '*', ' ', '*', ' ', ' '];
-    let row_3: Vec<char> = vec![' ', '*', ' ', '*', ' ', '*', '*', '*', ' ', ' '];
-    let row_4: Vec<char> = vec!['*', '*', ' ', '*', ' ', '*', ' ', ' ', '*', ' '];
-    let row_5: Vec<char> = vec!['*', '*', ' ', ' ', '*', '*', '*', ' ', '*', ' '];
-    let row_6: Vec<char> = vec!['*', ' ', ' ', '*', '*', ' ', ' ', '*', '*', ' '];
-    let row_7: Vec<char> = vec![' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*'];
-    let row_8: Vec<char> = vec![' ', '*', '*', ' ', '*', ' ', '*', ' ', ' ', '*'];
-    let row_9: Vec<char> = vec![' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*'];
+    let row_0: Vec<char> = vec![
+        '*', ' ', '*', '*', '*', '*', '*', ' ', ' ', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_1: Vec<char> = vec![
+        ' ', '*', ' ', '*', ' ', '*', '*', '*', ' ', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_2: Vec<char> = vec![
+        '*', ' ', '*', '*', ' ', '*', ' ', '*', ' ', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_3: Vec<char> = vec![
+        ' ', '*', ' ', '*', ' ', '*', '*', '*', ' ', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_4: Vec<char> = vec![
+        '*', '*', ' ', '*', ' ', '*', ' ', ' ', '*', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_5: Vec<char> = vec![
+        '*', '*', ' ', ' ', '*', '*', '*', ' ', '*', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_6: Vec<char> = vec![
+        '*', ' ', ' ', '*', '*', ' ', ' ', '*', '*', ' ', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_7: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_8: Vec<char> = vec![
+        ' ', '*', '*', ' ', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_9: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_10: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_11: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_12: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_13: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_14: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_15: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_16: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_17: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_18: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
+    let row_19: Vec<char> = vec![
+        ' ', '*', '*', '*', '*', ' ', '*', ' ', ' ', '*', ' ', '*', '*', '*', '*', ' ', '*', ' ',
+        ' ', '*',
+    ];
 
     let grid: Vec<Vec<char>> = vec![
-        row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9,
+        row_0, row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9, row_10, row_11,
+        row_12, row_13, row_14, row_15, row_16, row_17, row_18, row_19,
     ];
     return grid;
 }
@@ -83,19 +141,15 @@ fn std_out_grid(grid: &Vec<Vec<char>>) -> () {
     }
 }
 
-fn find_neighbour(
-    x: usize,
-    y: usize,
-    grid: &Vec<Vec<char>>,
-    edge_cat: usize,
-) -> [char; 8] {
+fn find_neighbour(x: usize, y: usize, grid: &Vec<Vec<char>>, edge_cat: usize) -> [char; 8] {
     let focus_cell = grid[y][x];
-    println!("focus_cell ~~> {}", focus_cell);
+    // println!("focus_cell ~~> {}", focus_cell);
 
     // *** My tuple template *** PS: Now an array!
     // (r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell)
 
-    if edge_cat == 0 {       // Top-left corner
+    if edge_cat == 0 {
+        // Top-left corner
         let r_cell = grid[y][x + 1];
         let l_cell = 'v';
         let d_cell = grid[y + 1][x];
@@ -108,20 +162,22 @@ fn find_neighbour(
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 1 {        // Top-Right corner
+    } else if edge_cat == 1 {
+        // Top-Right corner
         let r_cell = 'v';
         let l_cell = grid[y][x - 1];
-        let d_cell = grid[y+1][x];
+        let d_cell = grid[y + 1][x];
         let u_cell = 'v';
         let rd_cell = 'v';
-        let ld_cell = grid[y+1][x-1];
+        let ld_cell = grid[y + 1][x - 1];
         let ru_cell = 'v';
         let lu_cell = 'v';
 
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 2 {        // Bot-Right corner
+    } else if edge_cat == 2 {
+        // Bot-Right corner
         let r_cell = 'v';
         let l_cell = grid[y][x - 1];
         let d_cell = 'v';
@@ -134,20 +190,22 @@ fn find_neighbour(
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 3 {        // Bot-Left corner
+    } else if edge_cat == 3 {
+        // Bot-Left corner
         let r_cell = grid[y][x + 1];
         let l_cell = 'v';
         let d_cell = 'v';
         let u_cell = grid[y - 1][x];
         let rd_cell = 'v';
         let ld_cell = 'v';
-        let ru_cell = grid[y-1][x + 1];
+        let ru_cell = grid[y - 1][x + 1];
         let lu_cell = 'v';
 
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 4 {      // Left-Edge
+    } else if edge_cat == 4 {
+        // Left-Edge
         let r_cell = grid[y][x + 1];
         let l_cell = 'v';
         let d_cell = grid[y + 1][x];
@@ -160,7 +218,8 @@ fn find_neighbour(
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 5 {      // Top-Edge
+    } else if edge_cat == 5 {
+        // Top-Edge
         let r_cell = grid[y][x + 1];
         let l_cell = grid[y][x - 1];
         let d_cell = grid[y + 1][x];
@@ -173,7 +232,8 @@ fn find_neighbour(
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 6 {        // Right-Edge
+    } else if edge_cat == 6 {
+        // Right-Edge
         let r_cell = 'v';
         let l_cell = grid[y][x - 1];
         let d_cell = grid[y + 1][x];
@@ -186,7 +246,8 @@ fn find_neighbour(
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else if edge_cat == 7 {        // Bottom-Edge
+    } else if edge_cat == 7 {
+        // Bottom-Edge
         let r_cell = grid[y][x + 1];
         let l_cell = grid[y][x - 1];
         let d_cell = 'v';
@@ -199,8 +260,8 @@ fn find_neighbour(
         return [
             r_cell, l_cell, d_cell, u_cell, rd_cell, ld_cell, ru_cell, lu_cell,
         ];
-    } else {        // Middle Values
-        println!("HALLLP IMMA PANICK");
+    } else {
+        // Middle Values
         let r_cell = grid[y][x + 1];
         let l_cell = grid[y][x - 1];
         let d_cell = grid[y + 1][x];
@@ -217,28 +278,34 @@ fn find_neighbour(
 }
 
 fn check_edge(x: usize, y: usize, len: usize) -> usize {
-    if x == 0 && y == 0 {       // Top-left corner
+    if x == 0 && y == 0 {
+        // Top-left corner
         println!("return = 0");
         return 0;
-    } else if x == len -1 && y == 0 {
-        return 1;       // Top-Right corner | done
-    } else if x == len - 1 && y == len - 1 {        // Bot-Right corner
+    } else if x == len - 1 && y == 0 {
+        return 1; // Top-Right corner | done
+    } else if x == len - 1 && y == len - 1 {
+        // Bot-Right corner
         return 2;
-    } else if x == 0  && y == len -1 {      // Bot-Left corner
+    } else if x == 0 && y == len - 1 {
+        // Bot-Left corner
         return 3; // | 3
-    } else if x == 0 && y > 0 && y < len - 1 {      // Left-Edge
-        println!("y is > 0");
+    } else if x == 0 && y > 0 && y < len - 1 {
+        // Left-Edge
         return 4;
-    } else if x > 0 && x < len - 1 && y == 0 {      // Top-Edge
+    } else if x > 0 && x < len - 1 && y == 0 {
+        // Top-Edge
         return 5;
-    } else if x == len - 1 && y < len - 1 && y > 0 {        // Right-Edge
+    } else if x == len - 1 && y < len - 1 && y > 0 {
+        // Right-Edge
         return 6;
-    } else if x < len - 1 && x > 0 && y == len - 1 {        // Bottom-Edge
+    } else if x < len - 1 && x > 0 && y == len - 1 {
+        // Bottom-Edge
         return 7;
-    } else {        // Middle Values
+    } else {
+        // Middle Values
         // Condition in this step =>
-        // x < len-1 && x > 0 && y < len-1 && y > 0 
-        println!("mid val");
+        // x < len-1 && x > 0 && y < len-1 && y > 0
         return 8;
     }
 }
@@ -246,10 +313,8 @@ fn check_edge(x: usize, y: usize, len: usize) -> usize {
 fn check_life(focus_cell: &char) -> i8 {
     if focus_cell == &'*' {
         return 1;
-
-    } else if focus_cell == &' '{
+    } else if focus_cell == &' ' {
         return 0;
-
     } else {
         return -1;
     }
@@ -258,37 +323,38 @@ fn check_life(focus_cell: &char) -> i8 {
 // *** TODAY ***
 // check_life()
 // check_fate()
-    // Takes (x, y, neighbours, check_life())
-        // * Rules
-        //  + Any live cell with fewer than two live neighbours dies, as if by underpopulation | 
-        //  + Any live cell with two or three live neighbours lives on to the next generation |
-        //  + Any live cell with more than three live neighbours dies, as if by overpopulation 
-        //  + Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
-    // iterate through check_life((..)) { return i_sum value }
-    // if i_sum < 2 ~~> SET grid[y][x] = "-"
-    // else if ...
-    // else | i_sum > 3 |
+// Takes (x, y, neighbours, check_life())
+// * Rules
+//  + Any live cell with fewer than two live neighbours dies, as if by underpopulation |
+//  + Any live cell with two or three live neighbours lives on to the next generation |
+//  + Any live cell with more than three live neighbours dies, as if by overpopulation
+//  + Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+// iterate through check_life((..)) { return i_sum value }
+// if i_sum < 2 ~~> SET grid[y][x] = "-"
+// else if ...
+// else | i_sum > 3 |
 
-fn check_fate(x: usize, y: usize, neighbours: &[char; 8], mut grid: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn check_fate(
+    x: usize,
+    y: usize,
+    neighbours: &[char; 8],
+    mut grid: Vec<Vec<char>>,
+) -> Vec<Vec<char>> {
     let mut i_sum = 0;
     for i in neighbours.iter() {
         if check_life(i) == 1 {
             i_sum = i_sum + 1;
-            println!("i_sum ~~> {}", i_sum);
+            // println!("i_sum ~~> {}", i_sum);
         }
     }
     if i_sum < 2 {
         grid[y][x] = ' ';
-
     } else if i_sum >= 2 && i_sum <= 3 {
         grid[y][x] = '*';
-
     } else if i_sum > 3 {
         grid[y][x] = ' ';
-
     } else if i_sum == 3 {
         grid[y][x] = '*';
-
     }
     return grid;
 }
